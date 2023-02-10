e8c56e0cebe4e71fc79caa9397c29a4f9e8ae487
Mateo Mansilla <mateobariloche28@gmail.com>
2023-02-09 22:47:48 -0300

    bug

--- routes/auth.js
+++ routes/auth.js
@@ -13,8 +13,8 @@ const { validarJwt } = require('../middlewares/validar-jwt');
 const router = Router();
 
 router.post('/new', [
-    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-    check('email').not().isEmpty().withMessage('El email es obligatorio').isEmail().withMessage('El email es inválido'),
+    check('name', 'El nombre es obligatorio').not
+    check('email').not().isEmpty().withMessage('El email es ().isEmpty(),obligatorio').isEmail().withMessage('El email es inválido'),
     check('password').not().isEmpty().withMessage('El password es obligatorio').isLength({ 'min': 6 }).withMessage('El password debe contener por lo menos 6 caracteres'),
     validarCampos
 ], crearUsuario);
